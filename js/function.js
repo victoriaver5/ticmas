9bc048f4a0fdfb004f042ef7bcc0aa3157fd9959
victoria verdenelli <victoriaverdenelli@gmail.com>
2023-02-22 22:11:18 -0300

    commit final cv ticmas

--- js/function.js
+++ js/function.js
@@ -11,8 +11,28 @@ closemenuBtn.addEventListener("click",toggleMenu);
 
 const menuLinks= document.querySelectorAll(".menu a[href ^= '#']");
 
+const observer= new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+       const id = entry.target.getAttribute("id");
+       const menuLink = document.querySelector('.menu a[href="#${id}"]')
+
+       if (entry.isIntersecting){
+        document.querySelector(".menu a.selected").classList.remove("selected")
+        menuLink.classList.add("selected")
+       } 
+    });
+},
+    {rootMargin:"-40% 0px -70% 0px"}
+);
+
 menuLinks.forEach(menuLinks =>{
     menuLinks.addEventListener("click",function(){
         Menu.classList.remove("menu_opened");
     })
+
+    const hash = menuLinks.getAttribute("href");
+    const target = document.querySelector (hash);
+    if (target){
+        observer.observe(target);
+    }
 })
